68cc27324d0e36020cada1cfa5a68f615c848367
adospace <adospace@msn.com>
2020-11-09 13:04:17 +0100

    Host application setup

---
+++ src/WpfReactorUI.DemoApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace WpfReactorUI.DemoApp
+{
+    class Program
+    {
+        //[STAThread]
+        //public static void Main(string[] args)
+        //{
+        //    var app = new App();            
+        //    app.Run();
+        //}
+    }
+}
--- src/WpfReactorUI.Host/Program.cs
+++ src/WpfReactorUI.Host/Program.cs
@@ -1,12 +1,72 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using WpfReactorUI.Internals;
 
 namespace WpfReactorUI.Host
 {
     class Program
     {
-        static void Main(string[] args)
+        [STAThread]
+        public static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+#if DEBUG
+            if (args == null ||
+                args.Length == 0)
+            {
+                args = new[] { @"AvaloniaReactorUI.DemoApp.dll" };
+            }
+#endif
+            if (args == null ||
+                args.Length == 0)
+            {
+                Console.WriteLine("Specify the assembly path to load");
+                return -1;
+            }
+
+            var assemblyPath = args[0];
+
+            if (!Path.IsPathRooted(assemblyPath))
+            {
+                assemblyPath = Path.Combine(
+                    Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), assemblyPath);
+            }
+
+            if (!File.Exists(assemblyPath))
+            {
+                Console.WriteLine($"The assembly '{assemblyPath}' doesn't exist");
+                return -1;
+            }
+
+            var assemblyPdbPath = Path.Combine(Path.GetDirectoryName(assemblyPath), Path.GetFileNameWithoutExtension(assemblyPath) + ".pdb");
+
+            var assembly = File.Exists(assemblyPdbPath) ?
+                Assembly.Load(File.ReadAllBytes(assemblyPath))
+                :
+                Assembly.Load(File.ReadAllBytes(assemblyPath), File.ReadAllBytes(assemblyPdbPath));
+
+            ComponentLoader.Instance = new AssemblyFileComponentLoader(assemblyPath);
+
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+            string folderPath = Path.GetDirectoryName(assemblyPath);
+
+            currentDomain.AssemblyResolve += (object sender, ResolveEventArgs args) =>
+            {
+                string assemblyPath = Path.Combine(folderPath, new AssemblyName(args.Name).Name + ".dll");
+                if (!File.Exists(assemblyPath)) return null;
+                Assembly assembly = Assembly.LoadFrom(assemblyPath);
+                return assembly;
+            };
+
+            assembly
+                .GetTypes()
+                .First(_ => typeof(System.Windows.Application).IsAssignableFrom(_))
+                .GetMethod("Main")
+                .Invoke(null, null);
+
+            return 0;
         }
+
     }
 }
